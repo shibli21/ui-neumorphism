10eb78194afff73d5ee724e583864d0c46b86761
shibli21 <syedshiblimahmud@gmail.com>
2023-04-07 20:55:52 +0600

    updated to  react@18

--- example/src/index.js
+++ example/src/index.js
@@ -1,13 +1,15 @@
 import './index.css'
 
 import React from 'react'
-import ReactDOM from 'react-dom'
+import { createRoot } from 'react-dom/client'
 import App from './App'
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom'
 
-ReactDOM.render(
-  <Router basename={'/ui-neumorphism'}>
+const container = document.getElementById('root')
+const root = createRoot(container)
+
+root.render(
+  <BrowserRouter basename='/ui-neumorphism'>
     <App />
-  </Router>,
-  document.getElementById('root')
+  </BrowserRouter>
 )
