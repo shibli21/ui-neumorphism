b42c24f99fa076ca8910a5b07ebb76833c6d76d5
shibli21 <syedshiblimahmud@gmail.com>
2023-04-13 22:31:52 +0600

    parallax view

--- example/src/docs/code/parallax-code.ts
+++ example/src/docs/code/parallax-code.ts
@@ -1,4 +1,4 @@
-export const simpleParallax = (dark, props) => {
+export const simpleParallax = (dark: boolean, props: string) => {
   const p = props ? props + ' ' : ''
   return `<Parallax src='images/dessert-night.jpg' containerId='mainView' ${p}/>`
 }
